0207e906b4b943560b2d09b2fe9baa235f9e8cc1
Eddie Khae <103252363+eddward4040@users.noreply.github.com>
2024-07-08 06:34:14 +0000

    Create Task 1: Login Screen   Form1.cs

---
+++ Task 1: Login Screen   Form1.cs
@@ -0,0 +1,18 @@
+namespace LoginScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            MessageBox.Show($"Username: {username}, Password: {password}");
+        }
+    }
+}
